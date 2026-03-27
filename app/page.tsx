c158abe32a3ff097a285fb87dc7d500c708d07c8
Guilherme Silva <guilhermewew088@gmail.com>
2026-03-26 23:10:29 -0300

    corrigindo erro do typescript

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [lojas, setLojas] = useState([]);
+  const [lojas, setLojas] = useState<any[]>([]);
 
   // Pede a lista de lojas pro garçom (Python) assim que entra no site
   useEffect(() => {
